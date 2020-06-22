0df7737989b0cdcb4af698497e5e59f8992c117f
minsi yang <yangminsi@gmail.com>
2020-06-22 16:17:26 +0100

    added test for returnText method, passed

--- note-model-test.js
+++ note-model-test.js
@@ -1,7 +1,15 @@
+
 function constructingWithTheGivenString() {
     let string = "My favorite language is JavaScript."
     let note = new Note(string);
     expect.isTrue(note.text === string);
   }
   constructingWithTheGivenString();
+
+  function hasMethodToReturnText() {
+    let string = "Chris's idea!!"
+    let note = new Note(string);
+    expect.isTrue(note.returnText() === string );
+  }
+  hasMethodToReturnText();
   
--- note-model.js
+++ note-model.js
@@ -2,5 +2,11 @@
     function Note (text) {
        this.text = text; 
     } 
+
+    Note.prototype.returnText = function() {
+        return this.text
+    }
+
+    
     exports.Note = Note;
 })(this);
