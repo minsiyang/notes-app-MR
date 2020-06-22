23c75e26d80dad9872706ff125f78ad397fbaa6f
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-22 16:52:24 +0100

    second test added for NoteList + #addNote method

--- src/note-list-model.js
+++ src/note-list-model.js
@@ -2,5 +2,9 @@
   function NoteList() {
     this.notes = [];
   }
+
+  NoteList.prototype.addNote = function (note) {
+    this.notes.push(note);
+  };
   exports.NoteList = NoteList;
 })(this);
--- tests/note-list-model-test.js
+++ tests/note-list-model-test.js
@@ -3,3 +3,11 @@ function constructsWithAnEmptyArray() {
   expect.isTrue(typeof noteList.notes === "object");
 }
 constructsWithAnEmptyArray();
+
+function canStoreNotesInNoteArray() {
+  let noteList = new NoteList();
+  let note = new Note("hello");
+  noteList.addNote(note);
+  expect.isTrue(noteList.notes[0] === note);
+}
+canStoreNotesInNoteArray();
