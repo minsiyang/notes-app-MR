b5145219035fa77d17bec5c65e8c60b298e4eb10
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-22 15:29:15 +0100

    cleaned up test file

--- test_without_framework/circle_test.js
+++ test_without_framework/circle_test.js
@@ -2,17 +2,16 @@ function testCircleRadiusDefaultsTo10() {
   let circle = new Circle();
   expect.isTrue(circle.radius === 10);
 }
+testCircleRadiusDefaultsTo10();
 
 function testIfCircleIsTrue() {
   let circle = new Circle();
   expect.isTrue(circle.isCircle);
 }
+testIfCircleIsTrue();
 
 function testIfFalse() {
   let circle = new Circle();
   expect.isFalse(circle.isCircle);
 }
-
-testCircleRadiusDefaultsTo10();
-testIfCircleIsTrue();
 testIfFalse();
