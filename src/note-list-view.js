42c88a5387d6497033da56784df65f18a28d96d3
Rachael <rachaelewins@hotmail.com>
2020-06-23 15:53:02 +0100

    Note-list-view, 1+ note works

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -7,12 +7,9 @@
     NoteListView.prototype.returnHTML = function() {
         var array = this.frame.returnNote()
         var output = ""
-        if (array.length == 1) {
-          output = `<ul><li><div>${array[0].returnText()}</div></li></ul>`
-        }
-        // array.forEach(function(note) {
-        //   output += `<ul><li><div>${note.returnText()}</div></li></ul>`
-        // })
+        array.forEach(function(note) {
+          output += `<ul><li><div>${note.returnText()}</div></li></ul>`
+        })
         return output
     }
     exports.NoteListView = NoteListView;
