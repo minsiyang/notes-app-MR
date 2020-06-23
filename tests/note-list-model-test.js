7e0b8e92424e98096de41e44d004740ddb7510d0
minsi yang <yangminsi@gmail.com>
2020-06-23 10:29:04 +0100

    Added testing length of the array

--- tests/note-list-model-test.js
+++ tests/note-list-model-test.js
@@ -15,6 +15,7 @@ function canReturnStoredNotesInNoteArray() {
   let noteList = new NoteList();
   noteList.addNote("note");
   noteList.addNote("anotherNote");
+  expect.isTrue(noteList.returnNote().length === 2)
   expect.isTrue(noteList.returnNote()[0].text === "note");
   expect.isTrue(noteList.returnNote()[1].text === "anotherNote");
 }
