bca432595488e15e27454540862415fcbbfd79de
Rachael <rachaelewins@hotmail.com>
2020-06-23 15:50:48 +0100

    Note-list-view, 1 note works

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,11 +1,19 @@
 
 (function(exports) {
     function NoteListView(notelist) {
-      this.frame = notelist;
+        this.frame = notelist;
     }
 
     NoteListView.prototype.returnHTML = function() {
-        return ""
+        var array = this.frame.returnNote()
+        var output = ""
+        if (array.length == 1) {
+          output = `<ul><li><div>${array[0].returnText()}</div></li></ul>`
+        }
+        // array.forEach(function(note) {
+        //   output += `<ul><li><div>${note.returnText()}</div></li></ul>`
+        // })
+        return output
     }
-  exports.NoteListView = NoteListView;
+    exports.NoteListView = NoteListView;
 })(this);
